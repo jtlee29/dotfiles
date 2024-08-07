dc888c5ede56dbd9076300eb566761accec0d14b
Justin Lee <73514174+jtlee29@users.noreply.github.com>
2024-08-07 11:32:37 -0400

    feat: more plugins for nvim

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,13 +2,13 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
 
-  config = function () 
+  config = function ()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
       ensure_installed = {"lua", "javascript"},
 
       highlight = { enable = true },
-      indent = { enable = true },  
+      indent = { enable = true },
     })
   end
  }
