fe9b3bb884df20f9139d95d0d7c9a106f247f42e
Justin Lee <73514174+jtlee29@users.noreply.github.com>
2024-08-07 19:44:17 -0400

    feat: leader gb to see git blame

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -5,5 +5,6 @@ return {
     require('gitsigns').setup()
 
     vim.keymap.set('n', "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+    vim.keymap.set('n', "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
   end
 }
