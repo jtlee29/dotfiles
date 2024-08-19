e4eafb9e3a4808461b0dd21b085bc401b8ca1d89
Justin Lee <73514174+jtlee29@users.noreply.github.com>
2024-08-19 19:41:50 -0400

    chore: move gitsigns

---
+++ nvim/.config/nvim/lua/custom/plugins/gitsigns.lua
@@ -0,0 +1,14 @@
+-- Adds git related signed to the gutter, as well as utilities for managing changines
+-- See `:help gitsigns` to configuration keys
+return {
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+  },
+}
