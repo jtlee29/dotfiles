e0217f3e2fccf4a27660f8d5a6458b467ecfd3a9
Justin Lee <73514174+jtlee29@users.noreply.github.com>
2024-08-19 01:01:43 -0400

    chore: install tsserer and rust_analyzer

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -578,14 +578,14 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        tsserver = {},
         --
 
         lua_ls = {
