f183dad863363d341528a78663a93aab303229e1
Justin Lee <73514174+jtlee29@users.noreply.github.com>
2024-08-27 16:41:47 -0400

    chore: altered lualine theme

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -2,37 +2,35 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
 
-  config = function()
-    require('lualine').setup {
-      options = {
-        theme = 'auto',
-        component_separators = '',
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = { 'neo-tree' },
+  opts = {
+    options = {
+      theme = 'auto',
+      component_separators = '',
+      section_separators = { left = '', right = '' },
+      disabled_filetypes = { 'neo-tree' },
+    },
+    sections = {
+      lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+      lualine_b = { 'branch' },
+      lualine_c = {
+        '%=',
+        'filename',
       },
-      sections = {
-        lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-        lualine_b = { 'branch' },
-        lualine_c = {
-          -- '%=',
-          'filename',
-        },
-        lualine_x = { 'diagnostics', 'searchcount' },
-        lualine_y = { 'filetype', 'encoding' },
-        lualine_z = {
-          { 'location', separator = { right = '' }, left_padding = 2 },
-        },
+      lualine_x = { 'diagnostics', 'searchcount' },
+      lualine_y = { 'filetype', 'encoding' },
+      lualine_z = {
+        { 'location', separator = { right = '' }, left_padding = 2 },
       },
-      inactive_sections = {
-        lualine_a = { 'filename' },
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = { 'location' },
-      },
-      tabline = {},
-      extensions = {},
-    }
-  end,
+    },
+    inactive_sections = {
+      lualine_a = { 'filename' },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = { 'location' },
+    },
+    tabline = {},
+    extensions = {},
+  },
 }
