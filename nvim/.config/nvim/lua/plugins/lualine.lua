1a7204698871c810c8ca0c9cb52fc2610a92ab7e
Justin Lee <73514174+jtlee29@users.noreply.github.com>
2024-09-19 14:36:31 -0400

    chore: switch to square for laualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -6,19 +6,33 @@ return {
     options = {
       theme = 'auto',
       component_separators = '',
-      section_separators = { left = '', right = '' },
+      -- section_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
       disabled_filetypes = { 'neo-tree' },
+      globalstatus = true,
     },
     sections = {
       -- lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-      lualine_a = { { 'mode', right_padding = 2 } },
-      lualine_b = { 'branch' },
+      lualine_a = {
+        { 'mode', right_padding = 2 },
+      },
+      lualine_b = {
+        'branch',
+      },
       lualine_c = {
         '%=',
-        'filename',
+        { 'filename', path = 1 },
       },
-      lualine_x = { 'diagnostics', 'searchcount' },
-      lualine_y = { 'filetype', 'encoding' },
+
+      lualine_x = {
+        { 'diff', colored = true },
+        'diagnostics',
+        'searchcount',
+      },
+      lualine_y = {
+        { 'filetype', icon_only = true },
+        'encoding',
+      },
       lualine_z = {
         -- { 'location', separator = { right = '' }, left_padding = 2 },
         { 'location', left_padding = 2 },
