dba74cd8309b3492c05f0f47f206ef9b6ededd87
Justin Lee <73514174+jtlee29@users.noreply.github.com>
2025-02-23 13:52:34 -0500

    feat: base config of new-nvim config

---
+++ new-nvim/.config/nvim/init.lua
@@ -0,0 +1,46 @@
+--[[ Justin's Neovim Config ]]
+
+--  Set <space> as leader key
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.have_nerd_font = true
+
+-- Setup base files
+require("vim-opts")
+require("binds")
+require("auto-cmds")
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
+    { import = "themes" },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "rose-pine" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true, notify = false },
+})
+
+vim.cmd.colorscheme("rose-pine")
