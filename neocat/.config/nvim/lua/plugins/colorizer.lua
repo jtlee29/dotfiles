2a5104fab594178f93e81201b36b0eca946241d5
Justin Lee <73514174+jtlee29@users.noreply.github.com>
2025-06-11 19:40:37 -0400

    featL autotag and colorizer

---
+++ neocat/.config/nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,5 @@
+return {
+    "norcalli/nvim-colorizer.lua",
+    -- #ff5733
+    config = function() require("colorizer").setup() end,
+}
--- neocat/.config/nvim/lua/plugins/colorschemes.lua
+++ neocat/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,5 +1,6 @@
 return {
     "sainnhe/gruvbox-material",
+
     enabled = true,
     priority = 1000,
     config = function()
