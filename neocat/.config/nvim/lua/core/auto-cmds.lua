376783913c258cc31c4514518581ed9da3b1bdfc
Justin Lee <73514174+jtlee29@users.noreply.github.com>
2025-06-15 12:25:33 -0400

    chore: change to vim.hl.on_yank

--- neocat/.config/nvim/lua/core/auto-cmds.lua
+++ neocat/.config/nvim/lua/core/auto-cmds.lua
@@ -4,5 +4,5 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight when yanking (copying) text",
     group = vim.api.nvim_create_augroup("neocat-highlight-yank", { clear = true }),
-    callback = function() vim.highlight.on_yank() end,
+    callback = function() vim.hl.on_yank() end,
 })
