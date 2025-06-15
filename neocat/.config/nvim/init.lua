7b7d21d7d9fc875079e93c4b3f7e7a2bb2cb6f5c
Justin Lee <73514174+jtlee29@users.noreply.github.com>
2025-06-15 13:46:37 -0400

    fix: require lazy module first

--- neocat/.config/nvim/init.lua
+++ neocat/.config/nvim/init.lua
@@ -1,7 +1,8 @@
+-- Core Lazy module **MUST** be loaded first
+require("core.lazy")
 require("core.options")
 require("core.auto-cmds")
 require("core.binds")
-require("core.lazy")
 require("core.lsp")
 
 vim.cmd.colorscheme("gruvbox-material")
