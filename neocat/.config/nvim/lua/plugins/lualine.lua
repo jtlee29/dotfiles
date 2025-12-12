18778df9ac002496adef4253a19c3a07e7946529
Justin Lee <73514174+jtlee29@users.noreply.github.com>
2025-12-11 23:29:23 -0500

    feat: base lualine cofig

--- neocat/.config/nvim/lua/plugins/lualine.lua
+++ neocat/.config/nvim/lua/plugins/lualine.lua
@@ -6,13 +6,11 @@ return {
     options = {
       theme = "auto",
       component_separators = "",
-      -- section_separators = { left = '', right = '' },
       section_separators = { left = "", right = "" },
-      disabled_filetypes = { "neo-tree" },
+      disabled_filetypes = { "neo-tree", "snacks_dashboard" },
       globalstatus = true,
     },
     sections = {
-      -- lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
       lualine_a = {
         { "mode", right_padding = 2 },
       },
@@ -48,20 +46,18 @@ return {
       lualine_y = {
         { "diff", colored = true },
         { "filetype", icon_only = true },
-        "encoding",
       },
       lualine_z = {
-        -- { 'location', separator = { right = '' }, left_padding = 2 },
         { "location", left_padding = 2 },
       },
     },
     inactive_sections = {
-      lualine_a = { "filename" },
+      lualine_a = {},
       lualine_b = {},
       lualine_c = {},
       lualine_x = {},
       lualine_y = {},
-      lualine_z = { "location" },
+      lualine_z = {},
     },
     tabline = {},
     extensions = {},
